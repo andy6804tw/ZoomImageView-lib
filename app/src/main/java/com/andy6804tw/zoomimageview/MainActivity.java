7f3bcde9440aa814d1def62fa099b96da2883b44
andy6804tw <andy6804tw@yahoo.com.tw>
2017-07-06 18:47:37 +0800

    add Screenshot

--- app/src/main/java/com/andy6804tw/zoomimageview/MainActivity.java
+++ app/src/main/java/com/andy6804tw/zoomimageview/MainActivity.java
@@ -2,11 +2,9 @@ package com.andy6804tw.zoomimageview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView zoomImageView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
